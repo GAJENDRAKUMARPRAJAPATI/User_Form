493017c25427c7c855b33c091dd4b22df004f9cd
GAJENDRAKUMARPRAJAPATI <108794397+GAJENDRAKUMARPRAJAPATI@users.noreply.github.com>
2022-11-28 01:08:33 +0530

    Add files via upload
    Changes

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Form from './component/Form';
+
+ 
+ReactDom.render(
+   <>
+      <Form  />
+   </>
+
+    , document.getElementById('root')
+
+); 
